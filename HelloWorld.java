9b72b4273e87d0c1eb502a89d813c3b58475f310
dwi-ayu <dwiayuzaskia2@gmail.com>
2024-09-19 15:17:07 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+
+    public static void main(String args[] ) {
+	     System.out.println("Nama : Dwi Ayu Zaskia");
+		 System.out.println("NIM : 607062400034");
+		 System.out.println("Kelas : D3IF4801");
+	}
+}
